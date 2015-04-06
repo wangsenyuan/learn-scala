dec90e16b36dc4c898b9b3474153d21cf6282657
wangsenyuan <wang.senyuan@gmail.com>
2015-04-06 16:04:21 +0800

    two more leetcode cases

---
+++ src/main/leetcode/binarytreerightsideview/TreeNode.java
@@ -0,0 +1,13 @@
+package binarytreerightsideview;
+
+/**
+ * Created by senyuanwang on 15/4/6.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
---
+++ src/main/leetcode/houserobber/App.scala
@@ -0,0 +1,25 @@
+package houserobber
+
+/**
+ * Created by senyuanwang on 15/4/6.
+ */
+object App extends App {
+
+  def solve(num: Array[Int]): Int = {
+    if (num == null || num.length == 0) {
+      0
+    } else if (num.length == 1) {
+      num(0)
+    } else {
+      def go(i: Int, a: Int, b: Int): Int =
+        if (i == num.length) b
+        else {
+          val c = a + num(i)
+          go(i + 1, b, b max c)
+        }
+
+      go(2, num(0), num(0) max num(1))
+    }
+  }
+
+}
