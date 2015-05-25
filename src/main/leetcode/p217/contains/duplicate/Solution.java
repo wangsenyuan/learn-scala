72c121f57344b0de5e231b9bd3b0e719cb9162cf
wangsenyuan <wang.senyuan@gmail.com>
2015-05-25 16:17:10 +0800

    leetcode p217 contains duplidate

---
+++ src/main/leetcode/p217/contains/duplicate/Solution.java
@@ -0,0 +1,27 @@
+package p217.contains.duplicate;
+
+import java.util.BitSet;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by senyuanwang on 15/5/25.
+ */
+public class Solution {
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> bs = new HashSet<>(nums.length);
+
+        for (int x : nums) {
+            if (bs.contains(x)) {
+                return true;
+            }
+            bs.add(x);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(containsDuplicate(nums));
+    }
+}
