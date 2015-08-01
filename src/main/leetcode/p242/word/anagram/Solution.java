f1161b358c3d4d235879f88971b4390c53a72197
wangsenyuan <wang.senyuan@gmail.com>
2015-08-01 22:38:33 +0800

    leetcode 242

---
+++ src/main/leetcode/p242/word/anagram/Solution.java
@@ -0,0 +1,34 @@
+package p242.word.anagram;
+
+/**
+ * Created by senyuanwang on 15/8/1.
+ */
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] map = new int[26];
+
+        char[] cs = s.toCharArray();
+
+        for(char c : cs) {
+            int x = c - 'a';
+            map[x] += 1;
+        }
+
+        cs = t.toCharArray();
+
+        for(char c : cs) {
+            int x = c - 'a';
+            if(map[x] == 0) {
+                return false;
+            }
+            map[x] -= 1;
+        }
+
+        for(int x : map) {
+            if(x != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
