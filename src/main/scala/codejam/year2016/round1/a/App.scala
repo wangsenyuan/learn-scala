102bab4f530c19ffbcbfeac5d05dded34ee2805e
WangSenyuan <brucewang@maycur.com>
2016-04-30 17:56:14 +0800

    codejam year2016 round 1b solution

---
+++ src/main/scala/codejam/year2016/round1/a/App.scala
@@ -0,0 +1,38 @@
+package codejam.year2016.round1.a
+
+import scala.io.StdIn
+
+/**
+  * Created by wangsenyuan on 4/28/16.
+  */
+object App extends App {
+
+  def play(s: String): String = {
+    val sb = StringBuilder.newBuilder
+
+    for {
+      x <- s
+    } {
+      if (sb.isEmpty) {
+        sb.append(x)
+      } else {
+        if (x >= sb(0)) {
+          sb.insert(0, x)
+        } else {
+          sb.append(x)
+        }
+      }
+    }
+    return sb.toString()
+  }
+
+  val t = StdIn.readLine().toInt
+
+  for {
+    i <- 1 to t
+  } {
+    val s = StdIn.readLine()
+    val t = play(s)
+    println(s"Case #$i: $t")
+  }
+}
---
+++ src/main/scala/codejam/year2016/round1/b/App.scala
@@ -0,0 +1,41 @@
+package codejam.year2016.round1.b
+
+import scala.io.StdIn
+
+/**
+  * Created by wangsenyuan on 4/28/16.
+  */
+object App extends App {
+
+  def play(lists: Vector[Array[Int]], n: Int): Array[Int] = {
+    val counts = lists.flatMap(l => l).foldLeft(Map[Int, Int]()) {
+      (map, x) =>
+        map.get(x) match {
+          case None => map + (x -> 1)
+          case Some(y) => map + (x -> (y + 1))
+        }
+    }
+
+    val list = counts.filter(p => p._2 % 2 == 1).map(p => p._1)
+    list.toArray.sorted
+  }
+
+
+  var T = StdIn.readLine().toInt
+
+  for {
+    i <- 1 to T
+  } {
+    val n = StdIn.readLine().toInt
+    var rows = Vector[Array[Int]]()
+    for {
+      j <- 0 until 2 * n - 1
+    } {
+      val line = StdIn.readLine()
+      val row = line.split("\\s+").map(_.toInt)
+      rows = row +: rows
+    }
+    val missColumn = play(rows, n)
+    println(s"Case #$i: ${missColumn.mkString(" ")}")
+  }
+}
