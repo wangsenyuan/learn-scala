1c947688c8eabe3cedde4f4ee406bc45da1b9e1b
WangSenyuan <brucewang@maycur.com>
2016-05-09 20:29:45 +0800

    draft round3.c

---
+++ src/main/scala/codejam/year2016/round3/a/Small.scala
@@ -0,0 +1,11 @@
+package codejam.year2016.round3.a
+
+/**
+  * Created by wangsenyuan on 5/9/16.
+  */
+object Small extends App {
+
+  def play(parties: List[Int]): List[String] = {
+    parties.zip('A' to 'Z');
+  }
+}
