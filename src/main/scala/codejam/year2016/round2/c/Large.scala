9751d51dea8d0b33aecf5beaeeaf0e6c1a5879ea
WangSenyuan <brucewang@maycur.com>
2016-05-09 13:01:00 +0800

    codejam year2016 round2 c

---
+++ src/main/scala/codejam/year2016/round2/c/Large.scala
@@ -0,0 +1,102 @@
+package codejam.year2016.round2.c
+
+import scala.io.StdIn
+
+/**
+  * Created by wangsenyuan on 5/7/16.
+  */
+object Large extends App {
+
+  def play(topics: List[(String, String)]): Int = {
+    val g = new BipartiteGraph
+    for {
+      t <- topics
+      (f, s) = t
+    } {
+      g.connect(f, s)
+    }
+    topics.size - g.maxBPM
+  }
+
+  val T = StdIn.readLine().toInt
+
+  for {
+    i <- 1 to T
+  } {
+    val n = StdIn.readLine().toInt
+    var topics = List[(String, String)]()
+    for {
+      j <- 0 until n
+    } {
+      val line = StdIn.readLine().split("\\s+")
+      topics = (line(0), line(1)) :: topics
+    }
+    val r = play(topics)
+    println(s"Case #$i: $r")
+  }
+}
+
+class BipartiteGraph {
+
+  case class Node(v: String, edges: List[String]) {
+    def updateEdges(u: String): Node =
+      this.copy(edges = u :: edges)
+  }
+
+  var us = Map[String, Node]()
+  var vs = Map[String, Node]()
+
+  private def getNode(vs: Map[String, Node], v: String): Node = {
+    vs.get(v) match {
+      case None => Node(v, Nil)
+      case Some(n) => n
+    }
+  }
+
+  def connect(u: String, v: String): Unit = {
+    val uNode = getNode(us, u)
+    us += u -> uNode.updateEdges(v)
+
+    val vNode = getNode(vs, v)
+    vs += v -> vNode.updateEdges(u)
+  }
+
+
+  def maxBPM: Int = {
+    var pair = Map[String, String]()
+    def bpm(u: String, seen: Set[String]): Boolean = {
+      var success = false
+      for {
+        v <- us(u).edges
+        if !success && !seen(v)
+      } {
+        pair.get(v) match {
+          case None =>
+            pair += (v -> u)
+            success = true
+          case Some(w) if (bpm(w, seen + v)) =>
+            pair += (v -> u)
+            success = true
+          case _ =>
+        }
+      }
+      success
+    }
+
+    var r = 0
+    for {
+      u <- us.keys
+      if (bpm(u, Set()))
+    } {
+      r += 1
+    }
+
+    val pairedUsCnt = pair.values.size
+    val pairedVsCnt = pair.keySet.size
+
+    r + (us.size - pairedUsCnt) + (vs.size - pairedVsCnt)
+  }
+
+}
+
+
---
+++ src/main/scala/codejam/year2016/round2/c/Small.scala
@@ -0,0 +1,54 @@
+package codejam.year2016.round2.c
+
+import scala.io.StdIn
+
+/**
+  * Created by wangsenyuan on 5/6/16.
+  */
+object Small extends App {
+
+  case class Topic(first: String, second: String)
+
+  def play(topics: Vector[Topic]): Int = {
+    val firstSet = topics.map(_.first).toSet.size
+    val secondSet = topics.map(_.second).toSet.size
+
+    def findMinResult(results: Option[List[Topic]]*): Option[List[Topic]] = {
+      results.filter(_.isDefined).map(_.get).sortBy(_.size).headOption
+    }
+
+    def findUnFakeSet(result: List[Topic], list: List[Topic]): Option[List[Topic]] = {
+      val fs = result.map(_.first).toSet
+      val ss = result.map(_.second).toSet
+      if (fs.size == firstSet && ss.size == secondSet) {
+        Some(result)
+      } else list match {
+        case Nil => None
+        case h :: tail =>
+          val result1 = findUnFakeSet(h :: result, tail)
+          val result2 = findUnFakeSet(result, tail)
+          findMinResult(result1, result2)
+      }
+    }
+
+    topics.size - findUnFakeSet(Nil, topics.toList).get.size
+  }
+
+  val T = StdIn.readLine().toInt
+
+  for {
+    i <- 1 to T
+  } {
+    val n = StdIn.readLine().toInt
+    var topics = Vector[Topic]()
+    for {
+      j <- 0 until n
+    } {
+      val line = StdIn.readLine().split("\\s+")
+      val topic = Topic(line(0), line(1))
+      topics = topics :+ topic
+    }
+    val r = play(topics)
+    println(s"Case #$i: $r")
+  }
+}
