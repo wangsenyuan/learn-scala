4777a0cf43b8e4aae655e6f115beeb2960dbea36
WangSenyuan <brucewang@maycur.com>
2016-06-16 20:04:53 +0800

    aaaa

--- src/main/leetcode/p006/zigzag/Solution2.java
+++ src/main/leetcode/p006/zigzag/Solution2.java
@@ -10,42 +10,40 @@ public class Solution2 {
             return s;
         }
 
-        StringBuilder sb = new StringBuilder();
-
+        //        char[] as = s.toCharArray();
         int n = s.length();
+
+        char[] cs = new char[n];
 
         int[] p = {2 * (nRows - 1), 0};
         int x = 0;
         int j = 0;
         int r = 0;
-        //PAHNAPLSIIGYIR
-        //PAHNAPLSIIGYIR
         for (int i = 0; i < n; i++) {
-            sb.append(s.charAt(j));
+            cs[i] = s.charAt(j);
             if (j + p[x] >= n) {
                 j = ++r;
                 p[0] -= 2;
                 p[1] += 2;
                 x = 0;
                 if (p[x] == 0) {
-                    x = (x + 1) % 2;
+                    x = 1;
                 }
                 continue;
             }
 
             j += p[x];
-            x = (x + 1) % 2;
-            if (p[x] == 0) {
-                x = (x + 1) % 2;
+            if (p[1 - x] > 0) {
+                x = 1 - x;
             }
         }
 
-        return sb.toString();
+        return new String(cs);
     }
 
     public static void main(String[] args) {
-        //        System.out.println(convert("AB", 1));
-        //        System.out.println(convert("PAYPALISHIRING", 3));
+        System.out.println(convert("AB", 1));
+        System.out.println(convert("PAYPALISHIRING", 3));
 
         System.out.println(convert("ABCDEF", 4));
     }
