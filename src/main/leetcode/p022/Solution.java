d194456b05e6e255bc846e6fba7631ee407cf16f
wangsenyuan <wang.senyuan@gmail.com>
2016-07-03 17:33:08 +0800

    aaa

---
+++ src/main/leetcode/p022/Solution.java
@@ -0,0 +1,35 @@
+package p022;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by senyuanwang on 16/7/2.
+ */
+public class Solution {
+
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+
+        process(n, 0, new char[2 * n], 0, res);
+
+        return res;
+    }
+
+    private void process(int toOpen, int toClose, char[] cs, int i, List<String> res) {
+        if (i == cs.length) {
+            res.add(new String(cs));
+            return;
+        }
+
+        if (toOpen > 0) {
+            cs[i] = '(';
+            process(toOpen - 1, toClose + 1, cs, i + 1, res);
+        }
+
+        if (toClose > 0) {
+            cs[i] = ')';
+            process(toOpen, toClose - 1, cs, i + 1, res);
+        }
+    }
+}
