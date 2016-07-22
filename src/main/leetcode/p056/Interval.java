a3610ade3f84684e2d712591ae4ba9530628a9b1
WangSenyuan <brucewang@maycur.com>
2016-07-22 16:29:06 +0800

    aaa

---
+++ src/main/leetcode/p056/Interval.java
@@ -0,0 +1,19 @@
+package p056;
+
+/**
+ * Created by wangsenyuan on 7/15/16.
+ */
+public class Interval {
+    int start;
+    int end;
+
+    Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+}
