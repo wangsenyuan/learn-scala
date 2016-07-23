7d9719c72210bde8076a340b9da4298b801d4359
wangsenyuan <wang.senyuan@gmail.com>
2016-07-23 20:53:12 +0800

    aaa

---
+++ src/main/leetcode/p080/Solution.java
@@ -0,0 +1,22 @@
+package p080;
+
+/**
+ * Created by senyuanwang on 16/7/23.
+ */
+public class Solution {
+
+    public int removeDuplicates(int[] nums) {
+        int p = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (p >= 2 && nums[i] == nums[p - 1] && nums[p - 1] == nums[p - 2]) {
+                continue;
+            }
+
+            nums[p++] = nums[i];
+        }
+
+        return p;
+    }
+
+}
