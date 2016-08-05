9b1875dd61366812caf05dd890fcedf644e85445
WangSenyuan <brucewang@maycur.com>
2016-08-05 20:38:50 +0800

    aaa

---
+++ src/main/scala/codechef/horses/Main.scala
@@ -0,0 +1,27 @@
+package codechef.horses
+
+import scala.io.StdIn
+
+/**
+  * Created by wangsenyuan on 7/28/16.
+  */
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    var t = StdIn.readInt()
+
+    while (t > 0) {
+      val _ = StdIn.readInt()
+      val line = StdIn.readLine().split("\\s+").map(_.toInt).sorted.toList
+
+      val minDiff =
+        line.zip(line.tail).map {
+          case (a, b) => b - a
+        }.min
+
+      println(minDiff)
+
+      t -= 1
+    }
+  }
+}
