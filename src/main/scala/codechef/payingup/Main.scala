c1ed13c9efc6a83ebdf1412966e50e325b03a1ad
WangSenyuan <brucewang@maycur.com>
2016-08-10 20:51:21 +0800

    aaa

--- src/main/scala/codechef/payingup/Main.scala
+++ src/main/scala/codechef/payingup/Main.scala
@@ -1,16 +1,37 @@
 package codechef.payingup
 
+import scala.io.StdIn
+
 /**
   * Created by senyuanwang on 16/8/10.
   */
 object Main {
 
   def main(args: Array[String]): Unit = {
+    val T = StdIn.readInt()
+    var t = 0
+    while (t < T) {
+      val line = StdIn.readLine().split("\\s+").map(_.toInt)
+      val n = line(0)
+      val m = line(1)
 
+      val nums = Array.fill(n)(0)
+      var i = 0
+      while (i < n) {
+        nums(i) = StdIn.readInt()
+        i += 1
+      }
+
+      if (play(m, nums.sorted)) {
+        println("Yes")
+      } else {
+        println("No")
+      }
+      t += 1
+    }
   }
 
   def play(m: Int, nums: Array[Int]): Boolean = {
-    val sorted = nums.sorted
     val n = nums.length
     val sums = Array.fill(n + 1)(0)
 
@@ -23,19 +44,15 @@ object Main {
     def doPlay(i: Int, left: Int): Boolean = {
       if (left == 0 || left == sums(n) - sums(i)) {
         true
-      } else if (left < sorted(i)) {
+      } else if (left < nums(i)) {
         false
       } else if (left > sums(n) - sums(i)) {
         false
       } else {
-        doPlay(i + 1, left - sorted(i)) || doPlay(i + 1, left)
+        doPlay(i + 1, left - nums(i)) || doPlay(i + 1, left)
       }
     }
 
-    if (m == 0) {
-      false
-    } else {
-      doPlay(0, m)
-    }
+    m > 0 && doPlay(0, m)
   }
 }
