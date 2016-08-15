40f52949a8885edb4c829f25358635b194212495
WangSenyuan <brucewang@maycur.com>
2016-08-15 17:12:19 +0800

    aaa

--- src/main/scala/codechef/littleelephantandpermutations/Main.scala
+++ src/main/scala/codechef/littleelephantandpermutations/Main.scala
@@ -7,58 +7,20 @@ import scala.io.StdIn
   */
 object Main {
 
-  def at(len: Int, f: Int => Boolean): Int = {
+
+  def notPerfect(nums: Vector[Int]): Boolean = {
     var i = 0
-    var at = -1
-    while (i < len) {
-      if (f(i)) {
-        at = i
+    while (i < nums.size) {
+      var j = i + 2
+      while (j < nums.size) {
+        if (nums(i) > nums(j)) {
+          return true
+        }
+        j += 1
       }
       i += 1
     }
-
-    at
-  }
-
-  def maxAt(nums: Vector[Int]): Int = {
-    var a = Int.MinValue
-    at(nums.size, i => {
-      if (nums(i) > a) {
-        a = nums(i)
-        true
-      } else {
-        false
-      }
-    })
-  }
-
-  def minAt(nums: Vector[Int]): Int = {
-    var a = Int.MaxValue
-    at(nums.size, i => {
-      if (nums(i) < a) {
-        a = nums(i)
-        true
-      } else {
-        false
-      }
-    })
-  }
-
-  def notPerfect(nums: Vector[Int]): Boolean = {
-    if (nums.size <= 1) {
-      return false;
-    }
-
-    val mid = nums.size / 2
-
-    val (first, second) = nums.splitAt(mid)
-    val a = maxAt(first)
-    val b = minAt(second) + first.size
-    if (nums(a) > nums(b) && a + 1 < b) {
-      true
-    } else {
-      notPerfect(first) || notPerfect(second)
-    }
+    false
   }
 
   def main(args: Array[String]): Unit = {
