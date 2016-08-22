5cbbf9f26521f2ab69b04a4d7c1ac7b4a63745a7
WangSenyuan <brucewang@maycur.com>
2016-08-22 14:15:56 +0800

    aaa

---
+++ src/main/leetcode/p141/Solution.java
@@ -0,0 +1,41 @@
+package p141;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(0);
+        listNode.next = listNode;
+        Solution solution = new Solution();
+        System.out.println(solution.hasCycle(listNode));
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode a = head;
+        ListNode b = head;
+
+        while (a != null && b != null) {
+            a = a.next;
+            if (b.next == null) {
+                return false;
+            }
+            b = b.next.next;
+            if (a == b) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
