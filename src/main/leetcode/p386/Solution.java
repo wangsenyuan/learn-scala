de7155b53e9ab4ebb0b953c4b922f3f8582e37a6
wangsenyuan <wang.senyuan@gmail.com>
2016-08-24 21:35:08 +0800

    aaa

--- src/main/leetcode/p386/Solution.java
+++ src/main/leetcode/p386/Solution.java
@@ -15,25 +15,6 @@ public class Solution {
         System.out.println(result.size());
     }
 
-    public List<Integer> lexicalOrderQuick(int n) {
-        List<Integer> list = new ArrayList<>(n);
-        int curr = 1;
-        for (int i = 1; i <= n; i++) {
-            list.add(curr);
-            if (curr * 10 <= n) {
-                curr *= 10;
-            } else if (curr % 10 != 9 && curr + 1 <= n) {
-                curr++;
-            } else {
-                while ((curr / 10) % 10 == 9) {
-                    curr /= 10;
-                }
-                curr = curr / 10 + 1;
-            }
-        }
-        return list;
-    }
-
     public List<Integer> lexicalOrder(int n) {
         List<Integer> result = new ArrayList<>(n);
 
