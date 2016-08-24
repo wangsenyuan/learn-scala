005b9455ed56631fddc8da19039cd9a21412364f
wangsenyuan <wang.senyuan@gmail.com>
2016-08-24 21:21:48 +0800

    aaa

--- src/main/leetcode/p386/Solution.java
+++ src/main/leetcode/p386/Solution.java
@@ -9,10 +9,29 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        List<Integer> result = solution.lexicalOrder(999);
+        List<Integer> result = solution.lexicalOrderQuick(999);
         result.forEach(System.out::println);
         System.out.println("++++++++++");
         System.out.println(result.size());
+    }
+
+    public List<Integer> lexicalOrderQuick(int n) {
+        List<Integer> list = new ArrayList<>(n);
+        int curr = 1;
+        for (int i = 1; i <= n; i++) {
+            list.add(curr);
+            if (curr * 10 <= n) {
+                curr *= 10;
+            } else if (curr % 10 != 9 && curr + 1 <= n) {
+                curr++;
+            } else {
+                while ((curr / 10) % 10 == 9) {
+                    curr /= 10;
+                }
+                curr = curr / 10 + 1;
+            }
+        }
+        return list;
     }
 
     public List<Integer> lexicalOrder(int n) {
