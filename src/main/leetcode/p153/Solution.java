a74894e3d37aadc142c46ba0b5a1ce6ebe04e80d
WangSenyuan <brucewang@maycur.com>
2016-08-26 11:30:26 +0800

    aaa

--- src/main/leetcode/p153/Solution.java
+++ src/main/leetcode/p153/Solution.java
@@ -8,6 +8,11 @@ public class Solution {
     public int findMin(int[] nums) {
         int i = 0;
         int j = nums.length - 1;
+
+        while (i < j && nums[i] == nums[j]) {
+            j--;
+        }
+
         while (i < j) {
             int mid = (i + j) / 2;
             if (nums[mid] > nums[j]) {
