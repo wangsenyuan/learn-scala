c257bb2ea6b480564644043a2098f78a49fad9af
WangSenyuan <brucewang@maycur.com>
2016-08-27 16:45:06 +0800

    aaa

--- src/main/leetcode/p163/Solution.java
+++ src/main/leetcode/p163/Solution.java
@@ -11,24 +11,31 @@ public class Solution {
     public List<String> findMissingRanges(int[] a, int lo, int hi) {
         List<String> res = new ArrayList<>();
         int from = lo - 1;
+        StringBuilder sb = new StringBuilder();
         for (int num : a) {
             if (num < from) {
                 continue;
             }
+            sb.setLength(0);
 
             if (num - from > 2) {
-                res.add(String.format("%d->%d", from + 1, num - 1));
+                sb.append(from + 1).append("->").append(num - 1);
+                res.add(sb.toString());
             } else if (num - from == 2) {
-                res.add(String.format("%d", from + 1));
+                sb.append(from + 1);
+                res.add(sb.toString());
             }
 
             from = num;
         }
 
+        sb.setLength(0);
         if (hi - from >= 2) {
-            res.add(String.format("%d->%d", from + 1, hi));
+            sb.append(from + 1).append("->").append(hi);
+            res.add(sb.toString());
         } else if (hi - from == 1) {
-            res.add(String.format("%d", from + 1));
+            sb.append(from + 1);
+            res.add(sb.toString());
         }
 
         return res;
