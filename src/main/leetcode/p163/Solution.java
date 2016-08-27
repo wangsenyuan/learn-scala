a4656ca913a2c28c74eb4616fb4993d1d592ebdd
WangSenyuan <brucewang@maycur.com>
2016-08-27 16:32:49 +0800

    aaa

--- src/main/leetcode/p163/Solution.java
+++ src/main/leetcode/p163/Solution.java
@@ -9,36 +9,29 @@ import java.util.List;
 public class Solution {
 
     public List<String> findMissingRanges(int[] a, int lo, int hi) {
-        List<String> res = new ArrayList<String>();
-
-        // the next number we need to find
-        int next = lo;
-
-        for (int i = 0; i < a.length; i++) {
-            // not within the range yet
-            if (a[i] < next) continue;
-
-            // continue to find the next one
-            if (a[i] == next) {
-                next++;
+        List<String> res = new ArrayList<>();
+        int from = lo - 1;
+        for (int num : a) {
+            if (num < from) {
                 continue;
             }
 
-            // get the missing range string format
-            res.add(getRange(next, a[i] - 1));
+            if (num - from > 2) {
+                res.add(String.format("%d->%d", from + 1, num - 1));
+            } else if (num - from == 2) {
+                res.add(String.format("%d", from + 1));
+            }
 
-            // now we need to find the next number
-            next = a[i] + 1;
+            from = num;
         }
 
-        // do a final check
-        if (next <= hi) res.add(getRange(next, hi));
+        if (hi - from >= 2) {
+            res.add(String.format("%d->%d", from + 1, hi));
+        } else if (hi - from == 1) {
+            res.add(String.format("%d", from + 1));
+        }
 
         return res;
-    }
-
-    String getRange(int n1, int n2) {
-        return (n1 == n2) ? String.valueOf(n1) : String.format("%d->%d", n1, n2);
     }
 
 }
