ba5e6f3716654ad312241dc41259d8a58f009c09
WangSenyuan <brucewang@maycur.com>
2016-09-06 15:12:37 +0800

    aaa

--- src/main/leetcode/p190/Solution.java
+++ src/main/leetcode/p190/Solution.java
@@ -1,4 +1,4 @@
-package reversebits;
+package p190;
 
 /**
  * Created by senyuanwang on 15/3/8.
@@ -15,17 +15,14 @@ public class Solution {
     public static int reverseBits(int n) {
         int total = 32;
         for (int i = 0; i < total / 2; i++) {
-            n = swapBits(n, i, total - i - 1);
+            int j = total - i - 1;
+            int lo = (n >> i) & 1;
+            int hi = (n >> j) & 1;
+            if ((lo ^ hi) == 1) { //both bits are different so need to swap
+                n ^= ((1 << i) | (1 << j));
+            }
         }
         return n;
     }
 
-    private static int swapBits(int n, int i, int j) {
-        int lo = (n >> i) & 1;
-        int hi = (n >> j) & 1;
-        if ((lo ^ hi) == 1) { //both bits are different so need to swap
-            n ^= ((1 << i) | (1 << j));
-        }
-        return n;
-    }
 }
