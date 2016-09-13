848a384c812a8f15dd2d95d0ad709c33ba14388e
WangSenyuan <brucewang@maycur.com>
2016-09-13 10:14:32 +0800

    aaa

---
+++ src/main/leetcode/p398/Solution.java
@@ -0,0 +1,33 @@
+package p398;
+
+import java.util.Random;
+
+/**
+ * Created by wangsenyuan on 9/12/16.
+ */
+public class Solution {
+    private int[] nums;
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+    }
+
+    private Random r = new Random();
+
+    public int pick(int target) {
+        int ret = -1;
+        if (nums == null) {
+            return ret;
+        }
+        int upbound = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                if (r.nextInt(upbound) == 0) {
+                    ret = i;
+                }
+                upbound++;
+            }
+        }
+        return ret;
+    }
+}
