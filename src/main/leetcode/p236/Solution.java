1eb62f020432a6a11d548fd0102b41e9ffc7f949
WangSenyuan <brucewang@maycur.com>
2016-09-21 15:27:21 +0800

    aaa

--- src/main/leetcode/p236/Solution.java
+++ src/main/leetcode/p236/Solution.java
@@ -2,32 +2,34 @@ package p236;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 
 /**
  * Created by senyuanwang on 15/7/13.
  */
 public class Solution {
 
-    private boolean findNode(TreeNode cur, TreeNode target, List<TreeNode> path) {
+    static class Result extends RuntimeException {
+        final List<TreeNode> path;
+
+        Result(List<TreeNode> path) {
+            this.path = path;
+        }
+    }
+
+    private void findNode(TreeNode cur, TreeNode target, List<TreeNode> path) {
         if (cur == target) {
             path.add(target);
-            return true;
+            throw new Result(path);
         }
         if (cur == null) {
-            return false;
+            return;
         }
+        path.add(cur);
 
-        if (findNode(cur.left, target, path)) {
-            path.add(cur);
-            return true;
-        }
+        findNode(cur.left, target, path);
+        findNode(cur.right, target, path);
 
-        if (findNode(cur.right, target, path)) {
-            path.add(cur);
-            return true;
-        }
-        return false;
+        path.remove(path.size() - 1);
     }
 
 
@@ -37,39 +39,42 @@ public class Solution {
         List<TreeNode> qAncestors = ancestors(root, q);
 
         if (pAncestors.size() < qAncestors.size()) {
-            return lca(pAncestors, qAncestors.subList(qAncestors.size() - pAncestors.size(), qAncestors.size()));
+            return lca(pAncestors, qAncestors, pAncestors.size());
         }
-        return lca(pAncestors.subList(pAncestors.size() - qAncestors.size(), pAncestors.size()), qAncestors);
+        return lca(pAncestors, qAncestors, qAncestors.size());
     }
 
-    private TreeNode lca(List<TreeNode> as, List<TreeNode> bs) {
+    private TreeNode lca(List<TreeNode> as, List<TreeNode> bs, int len) {
         int i = 0;
-        int j = as.size();
+        int j = len - 1;
         while (i <= j) {
             int k = i + (j - i) / 2;
             if (as.get(k) == bs.get(k)) {
-                j = k - 1;
-            } else {
                 i = k + 1;
+            } else {
+                j = k - 1;
             }
         }
-        return as.get(j + 1);
+        return as.get(i - 1);
     }
 
     private List<TreeNode> ancestors(TreeNode root, TreeNode p) {
-        List<TreeNode> pAncestors = new ArrayList<>();
-        findNode(root, p, pAncestors);
-        pAncestors.add(root);
-        return pAncestors;
+        List<TreeNode> ancestors = new ArrayList<>();
+        try {
+            findNode(root, p, ancestors);
+            return null;
+        } catch (Result result) {
+            return result.path;
+        }
     }
 
 
 
     public static void main(String[] args) {
-        TreeNode a = new TreeNode(2);
-        TreeNode b = new TreeNode(1);
-        b.right = a;
-        System.out.println(new Solution().lowestCommonAncestor(b, a, b).val);
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.left = new TreeNode(3);
+        System.out.println(new Solution().lowestCommonAncestor(root, root.left, root.right).val);
     }
 }
 
