597612964cc18729c5d7cb74f7486f5bbda998a7
wangsenyuan <wang.senyuan@gmail.com>
2016-09-25 12:52:07 +0800

    aaa

---
+++ src/main/scala/com/me/learn/ImplicitEvidence.scala
@@ -0,0 +1,25 @@
+package com.me.learn
+
+/**
+  * Created by senyuanwang on 16/9/24.
+  */
+object ImplicitEvidence extends App {
+
+  object M {
+    implicit object AMarker
+    implicit object BMarker
+
+    def m(ints: Seq[Int])(implicit i: AMarker.type ): Unit = {
+      println(s"int seq $ints");
+    }
+
+    def m(strs: Seq[String])(implicit s: BMarker.type ): Unit = {
+      println(s"string seq $strs")
+    }
+  }
+
+  import M._
+
+  m(Seq(1, 2, 3))
+  m(Seq("a", "b", "c"))
+}
---
+++ src/main/scala/com/me/learn/ToMapDemo.scala
@@ -0,0 +1,10 @@
+package com.me.learn
+
+/**
+  * Created by senyuanwang on 16/9/22.
+  */
+object ToMapDemo extends App {
+
+  val list = List('a' -> 1, 'b' -> 2)
+  val map = list.toMap
+}
