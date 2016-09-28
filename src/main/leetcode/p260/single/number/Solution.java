956f90558cf4f5dd1cba5911e528da8958f52154
WangSenyuan <brucewang@maycur.com>
2016-09-28 11:37:07 +0800

    aaa

--- src/main/leetcode/p260/single/number/Solution.java
+++ src/main/leetcode/p260/single/number/Solution.java
@@ -17,14 +17,20 @@ public class Solution {
         // Pass 2 :
         int[] rets = {0, 0}; // this array stores the two numbers we will return
         for (int num : nums) {
-            if ((num & diff) == 0) // the bit is not set
-            {
+            if ((num & diff) == 0) {
                 rets[0] ^= num;
-            } else // the bit is set
-            {
+            } else {
                 rets[1] ^= num;
             }
         }
         return rets;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 1, 3, 2, 5};
+        Solution solution = new Solution();
+        int[] res = solution.singleNumber(nums);
+        System.out.println(res[0]);
+        System.out.println(res[1]);
     }
 }
