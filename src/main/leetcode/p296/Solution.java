5444916af0eee95e64c7314a0e8feeb07344c49e
wangsenyuan <wang.senyuan@gmail.com>
2016-10-11 22:43:24 +0800

    aaa

--- src/main/leetcode/p296/Solution.java
+++ src/main/leetcode/p296/Solution.java
@@ -1,74 +1,50 @@
 package p296;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by wangsenyuan on 11/10/2016.
  */
 public class Solution {
-
     public int minTotalDistance(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] people = new int[m][n];
+        List<Integer> rows = collectRows(grid);
+        List<Integer> cols = collectCols(grid);
+        int row = rows.get(rows.size() / 2);
+        int col = cols.get(cols.size() / 2);
+        return minDistance1D(rows, row) + minDistance1D(cols, col);
+    }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int x = grid[i][j];
-                if (i > 0) {
-                    x += people[i - 1][j];
-                }
-                if (j > 0) {
-                    x += people[i][j - 1];
-                }
-                if (i > 0 && j > 0) {
-                    x -= people[i - 1][j - 1];
-                }
-                people[i][j] = x;
-            }
+    private int minDistance1D(List<Integer> points, int origin) {
+        int distance = 0;
+        for (int point : points) {
+            distance += Math.abs(point - origin);
         }
+        return distance;
+    }
 
-        int[][] f = new int[m][n];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i > 0) {
-                    f[i][j] += f[i - 1][j] + people[i - 1][j];
-                }
-                if (j > 0) {
-                    f[i][j] += f[i][j - 1] + people[i][j - 1];
-                }
-                if (i > 0 && j > 0) {
-                    f[i][j] -= people[i - 1][j - 1];
+    private List<Integer> collectRows(int[][] grid) {
+        List<Integer> rows = new ArrayList<>();
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                if (grid[row][col] == 1) {
+                    rows.add(row);
                 }
             }
         }
-        int[][] g = new int[m][n];
+        return rows;
+    }
 
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (i < m - 1) {
-                    g[i][j] +=
-                        g[i + 1][j] + people[m - 1][n - 1] + people[i + 1][j] - people[m - 1][j] - people[i + 1][n - 1];
-                }
-                if (j < n - 1) {
-                    g[i][j] +=
-                        g[i][j + 1] + people[m - 1][n - 1] + people[i][j + 1] - people[m - 1][j + 1] - people[i][n - 1];
-                }
-
-                if (i < m - 1 && j < n - 1) {
-                    g[i][j] -=
-                        people[m - 1][n - 1] + people[i + 1][j + 1] - people[m - 1][j + 1] - people[i + 1][n - 1];
+    private List<Integer> collectCols(int[][] grid) {
+        List<Integer> cols = new ArrayList<>();
+        for (int col = 0; col < grid[0].length; col++) {
+            for (int row = 0; row < grid.length; row++) {
+                if (grid[row][col] == 1) {
+                    cols.add(col);
                 }
             }
         }
-
-        int res = Integer.MAX_VALUE;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (f[i][j] + g[i][j] < res) {
-                    res = f[i][j] + g[i][j];
-                }
-            }
-        }
-        return res;
+        return cols;
     }
 }
