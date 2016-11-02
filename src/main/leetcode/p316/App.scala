622d398bbc1ad5b8d4e13c5a9edae226d7957910
WangSenyuan <brucewang@maycur.com>
2016-11-02 20:21:45 +0800

    aaaa

---
+++ src/main/leetcode/p316/App.scala
@@ -0,0 +1,16 @@
+package p316
+
+/**
+  * Created by wangsenyuan on 02/11/2016.
+  */
+object App {
+
+  def main(args: Array[String]): Unit = {
+    println(removeDuplicateLetters("bcabc"))
+    println(removeDuplicateLetters("cbacdcbc"))
+  }
+
+  def removeDuplicateLetters(s: String): String = {
+    ???
+  }
+}
