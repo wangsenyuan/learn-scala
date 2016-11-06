230fe1da481f91607eac60aa0c981cc60ae8a1f3
WangSenyuan <brucewang@maycur.com>
2016-11-06 08:49:57 +0800

    aaa

--- src/main/leetcode/p324/Solution.java
+++ src/main/leetcode/p324/Solution.java
@@ -1,5 +1,6 @@
 package p324;
 
+import java.util.Random;
 import java.util.stream.IntStream;
 
 /**
@@ -17,11 +18,14 @@ public class Solution {
         int n = nums.length;
         if (n < 2)
             return;
+        Random random = new Random(41);
         // find median
         int i = 0, j = n - 1;
         int median = 0;
         while (i <= j) {
             int p = i, a = i + 1, b = j;
+            int c = random.nextInt(j - i + 1) + i;
+            swap(nums, p, c);
             while (a <= b) {
                 if (nums[a] >= nums[p])
                     swap(nums, a, b--);
