eaeeed38d2b344e818ce4499fbb57ae0ddb45e79
WangSenyuan <brucewang@maycur.com>
2016-11-09 09:49:17 +0800

    aaa

--- src/main/leetcode/p327/Solution4.java
+++ src/main/leetcode/p327/Solution4.java
@@ -1,10 +1,20 @@
 package p327;
 
+import java.util.Arrays;
+
 /**
  * Created by wangsenyuan on 08/11/2016.
  */
 public class Solution4 {
 
+
+    public static void main(String[] args) {
+        Solution4 solution = new Solution4();
+        int[] nums = {-2, 5, -1};
+        int upper = 2;
+        int lower = -2;
+        System.out.println(solution.countRangeSum(nums, lower, upper));
+    }
 
     public int countRangeSum(int[] nums, int lower, int upper) {
         if (nums == null || nums.length == 0 || lower > upper) {
@@ -17,37 +27,32 @@ public class Solution4 {
             prefixArray[i] = prefixArray[i - 1] + nums[i - 1];
         }
 
-        return countRangeSumSubI(prefixArray, 0, prefixArray.length - 1, lower, upper);
+        return countRangeSumSubI(prefixArray, 0, prefixArray.length, lower, upper);
     }
 
     private int countRangeSumSubI(long[] prefixArray, int l, int r, int lower, int upper) {
-        if (l >= r)
-            return 0;
-
         int m = l + (r - l) / 2;
-
-        int count =
-            countRangeSumSubI(prefixArray, l, m, lower, upper) + countRangeSumSubI(prefixArray, m + 1, r, lower, upper);
-
-        long[] mergedArray = new long[r - l + 1];
-        int i = l, j = m + 1, k = m + 1, p = 0, q = m + 1;
-
-        while (i <= m) {
-            while (j <= r && prefixArray[j] - prefixArray[i] < lower)
-                j++;
-            while (k <= r && prefixArray[k] - prefixArray[i] <= upper)
-                k++;
-            count += k - j;
-
-            while (q <= r && prefixArray[q] < prefixArray[i])
-                mergedArray[p++] = prefixArray[q++];
-            mergedArray[p++] = prefixArray[i++];
+        if (m == l) {
+            return 0;
         }
 
-        while (q <= r)
-            mergedArray[p++] = prefixArray[q++];
+        int count =
+            countRangeSumSubI(prefixArray, l, m, lower, upper) + countRangeSumSubI(prefixArray, m, r, lower, upper);
 
-        System.arraycopy(mergedArray, 0, prefixArray, l, mergedArray.length);
+        int i = m;
+        int j = m;
+        for (int k = l; k < m; k++) {
+            long left = prefixArray[k];
+            while (i < r && prefixArray[i] - left < lower) {
+                i++;
+            }
+            while (j < r && prefixArray[j] - left <= upper) {
+                j++;
+            }
+            count += j - i;
+        }
+
+        Arrays.sort(prefixArray, l, r);
 
         return count;
     }
