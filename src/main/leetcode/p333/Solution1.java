65e772f8c162b7c5951f5f24dfa55c25c89dee88
WangSenyuan <brucewang@maycur.com>
2016-11-10 09:30:53 +0800

    aaa

---
+++ src/main/leetcode/p333/Solution1.java
@@ -0,0 +1,39 @@
+package p333;
+
+/**
+ * Created by wangsenyuan on 09/11/2016.
+ */
+public class Solution1 {
+
+    public int largestBSTSubtree(TreeNode root) {
+        if (isValid(root, Integer.MIN_VALUE, Integer.MAX_VALUE)) {
+            return count(root);
+        }
+
+        int left = largestBSTSubtree(root.left);
+        int right = largestBSTSubtree(root.right);
+
+        if (left > right) {
+            return left;
+        }
+        return right;
+    }
+
+    private boolean isValid(TreeNode root, int minVal, int maxVal) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val > maxVal || root.val < minVal) {
+            return false;
+        }
+        return isValid(root.left, minVal, root.val) && isValid(root.right, root.val, maxVal);
+    }
+
+    private int count(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + count(root.left) + count(root.right);
+    }
+}
