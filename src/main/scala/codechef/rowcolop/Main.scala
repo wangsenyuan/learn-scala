06f4676a2dd10b30dcc924ba5c30ac547dad0f8b
WangSenyuan <brucewang@maycur.com>
2016-12-16 10:02:29 +0800

    aaa

---
+++ src/main/scala/codechef/rowcolop/Main.scala
@@ -0,0 +1,32 @@
+package codechef.rowcolop
+
+import scala.io.StdIn
+
+/**
+  * Created by wangsenyuan on 15/12/2016.
+  */
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val firstLine = StdIn.readLine().split("\\s+").map(_.toInt)
+    val n = firstLine(0)
+    var q = firstLine(1)
+    val rows = Array.fill(n)(0L)
+    val cols = Array.fill(n)(0L)
+    while (q > 0) {
+      q -= 1
+
+      val line = StdIn.readLine().split("\\s+")
+      val i = line(1).toInt
+      val x = line(2).toInt
+      line(0) match {
+        case "RowAdd" =>
+          rows(i - 1) += x
+        case "ColAdd" =>
+          cols(i - 1) += x
+
+      }
+    }
+    println(rows.max + cols.max)
+  }
+}
