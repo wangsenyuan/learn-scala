13217a8224fc1af98d0e3bbab4d5483642ae172e
WangSenyuan <brucewang@maycur.com>
2017-02-24 17:36:50 +0800

    aaa

--- src/main/scala/codechef/easy/setdiff/Main.scala
+++ src/main/scala/codechef/easy/setdiff/Main.scala
@@ -8,7 +8,52 @@ import scala.io.StdIn
 object Main {
   val MOD = 1000000007
 
+  val N = 100000
+
+  def preComputePow2(n: Int) = {
+    val ps = Array.fill(n + 1)(1L)
+
+    (1 to n) foreach {
+      i =>
+        val j = i / 2
+        val x = ps(j)
+        val y = x * x % MOD
+        ps(i) = y
+        if (i % 2 == 1) {
+          ps(i) = y * 2 % MOD
+        }
+    }
+
+    ps
+  }
+
   def main(args: Array[String]): Unit = {
+    val p2 = preComputePow2(N)
+
+    val t = StdIn.readInt()
+
+    (0 until t) foreach {
+      _ =>
+        val _ = StdIn.readInt()
+        val nums = StdIn.readLine().split("\\s+").map(_.toInt).sorted
+        val n = nums.length
+
+        var res = 0L
+        (0 until n) foreach {
+          i =>
+            (i + 1 until n) foreach {
+              j =>
+                val sz = j - i + 1 - 2
+                res = (res + (nums(j).toLong - nums(i).toLong) * p2(sz) % MOD) % MOD
+            }
+        }
+
+        println(res)
+    }
+
+  }
+
+  def main2(args: Array[String]): Unit = {
     val t = StdIn.readInt()
 
     (0 until t) foreach {
