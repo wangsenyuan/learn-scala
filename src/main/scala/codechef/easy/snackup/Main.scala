6c37dcf3cba354ebbc7d860567b4dcf7176ab2e3
WangSenyuan <brucewang@maycur.com>
2017-06-21 20:32:54 +0800

    aaa

---
+++ src/main/scala/codechef/easy/snackup/Main.scala
@@ -0,0 +1,42 @@
+package codechef.easy.snackup
+
+import scala.io.StdIn
+
+/**
+  * Created by wangsenyuan on 21/06/2017.
+  */
+object Main {
+
+  def solve() = {
+    val n = StdIn.readInt()
+
+    def invite(x: Int, y: Int): Unit = {
+      var i = 0
+      while (i < n) {
+        val a = (x + i) % n
+        val b = (y + i) % n
+        println(s"${a + 1} ${b + 1} ${(b + 1) % n + 1}")
+        i += 1
+      }
+    }
+
+    println(n)
+
+    var i = 0
+    while (i < n) {
+      invite(i, 0)
+      i += 1
+    }
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    val t = StdIn.readInt()
+
+    var i = 0
+    while (i < t) {
+      solve()
+      i += 1
+    }
+  }
+}
