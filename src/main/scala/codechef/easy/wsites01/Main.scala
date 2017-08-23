a19df71fd7e358dcd1522fe1a29fbfaeb70b4b81
WangSenyuan <brucewang@maycur.com>
2017-08-23 14:35:01 +0800

    aaaa

--- src/main/scala/codechef/easy/wsites01/Main.scala
+++ src/main/scala/codechef/easy/wsites01/Main.scala
@@ -5,16 +5,49 @@ import scala.io.StdIn
 
 object Main {
 
+  sealed trait State
+
+  case object NoSite extends State
+
+  case object AllBlocked extends State
+
+  case object AllUnBlocked extends State
+
+  case object PartBlocked extends State
+
   class Trie {
     val children = Array.fill[Trie](26)(null)
-    var blocked: Option[Boolean] = None
+    var state: State = NoSite
+
+    private def updateState(blocked: Boolean): State = {
+      state match {
+        case NoSite =>
+          if (blocked) {
+            AllBlocked
+          } else {
+            AllUnBlocked
+          }
+        case AllBlocked =>
+          if (blocked) {
+            AllBlocked
+          } else {
+            PartBlocked
+          }
+        case AllUnBlocked =>
+          if (blocked) {
+            PartBlocked
+          } else {
+            AllUnBlocked
+          }
+        case PartBlocked =>
+          PartBlocked
+      }
+    }
 
     def addSite(site: String, i: Int, blocked: Boolean): Unit = {
+      state = updateState(blocked)
+
       if (i < site.length) {
-        this.blocked match {
-          case None => this.blocked = Some(blocked)
-          case Some(old) => this.blocked = Some(old && blocked)
-        }
         val c = site(i)
         if (children(c - 'a') == null) {
           children(c - 'a') = new Trie
@@ -23,23 +56,35 @@ object Main {
       }
     }
 
+    def hasSolution: Boolean = {
+      if (state == PartBlocked) {
+        val blockPrefixUnblock = children.filter(_ != null).foldLeft(true)((res, child) => res && (child.state == NoSite || child.state == AllUnBlocked))
+        if (blockPrefixUnblock) {
+          false
+        } else {
+          children.filter(_ != null).foldLeft(true)((res, child) => res && child.hasSolution)
+        }
+      } else {
+        children.filter(_ != null).foldLeft(true)((res, child) => res && child.hasSolution)
+      }
+    }
+
     def findBlockedFilters(path: String, res: ListBuffer[String]): Unit = {
-      this.blocked match {
-        case Some(v) =>
-          if (!v) {
-            var i = 0
-            while (i < children.length) {
+      state match {
+        case NoSite =>
+        case AllUnBlocked =>
+        case AllBlocked =>
+          res += path
+        case PartBlocked =>
+          var i = 0
+          while (i < children.length) {
 
-              if (children(i) != null) {
-                children(i).findBlockedFilters(path + (i + 'a').toChar, res)
-              }
-
-              i += 1
+            if (children(i) != null) {
+              children(i).findBlockedFilters(path + (i + 'a').toChar, res)
             }
-          } else {
-            res += path
+
+            i += 1
           }
-        case _ =>
       }
     }
   }
@@ -59,14 +104,16 @@ object Main {
       i += 1
     }
 
-    val res = ListBuffer.empty[String]
-    trie.findBlockedFilters("", res)
+    if (trie.hasSolution) {
+      val res = ListBuffer.empty[String]
+      trie.findBlockedFilters("", res)
 
-    if (res.size == 0) {
-      println(-1)
-    } else {
       println(res.size)
       res.foreach(println)
+    } else {
+      println(-1)
     }
+
+
   }
 }
