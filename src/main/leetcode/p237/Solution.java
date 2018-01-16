e2ce3fe35acfa938d139fa0fe5a38a4e6dd3e692
WangSenyuan <brucewang@maycur.com>
2018-01-16 21:55:56 +0800

    aaa

---
+++ src/main/leetcode/p237/Solution.java
@@ -0,0 +1,15 @@
+package p237;
+
+import common.ListNode;
+
+public class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode prev = null;
+        while (node.next != null) {
+            prev = node;
+            node.val = node.next.val;
+            node = node.next;
+        }
+        prev.next = null;
+    }
+}
