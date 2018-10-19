1f7f8b3615e2806b0efdd0dbc2fdf27fbd15e452
WangSenyuan <brucewang@maycur.com>
2018-10-19 14:26:54 +0800

    aaa

---
+++ src/test/scala/set000/set000/p001/SolutionTest.scala
@@ -0,0 +1,14 @@
+package set000.set000.p001
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionTest extends FlatSpec with Matchers {
+
+  it should "get correct answer" in {
+    val nums = Array(2, 7, 11, 15)
+    val target = 9
+    val expect = Array(0, 1)
+    val res = Solution.twoSum(nums, target)
+    res shouldEqual expect
+  }
+}
