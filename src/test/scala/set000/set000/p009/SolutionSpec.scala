6b8debd40ba014bef2ecdcd257b90870dd371295
WangSenyuan <brucewang@maycur.com>
2018-11-26 09:50:42 +0800

    leetcode p009

---
+++ src/test/scala/set000/set000/p009/SolutionSpec.scala
@@ -0,0 +1,20 @@
+package set000.set000.p009
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers {
+  it should "get true when check 121" in {
+    val res = Solution.isPalindrome(121)
+    res should be(true)
+  }
+
+  it should "get false when check -121" in {
+    val res = Solution.isPalindrome(-121)
+    res should be(false)
+  }
+
+  it should "get false when check 10" in {
+    val res = Solution.isPalindrome(10)
+    res should be(false)
+  }
+}
