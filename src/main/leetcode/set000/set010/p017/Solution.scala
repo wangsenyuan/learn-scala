c6070a17b31b3fcd759844dd3fdd4683c8117d99
WangSenyuan <brucewang@maycur.com>
2018-12-02 09:00:42 +0800

    aaa

---
+++ src/main/leetcode/set000/set010/p017/Solution.scala
@@ -0,0 +1,24 @@
+package set000.set010.p017
+
+object Solution {
+  val map = Array("", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz")
+
+  def letterCombinations(digits: String): List[String] = {
+    if (digits == null || digits.size == 0) {
+      Nil
+    } else {
+      def go(digits: String, prefix: String): IndexedSeq[String] = {
+        if (digits.isEmpty) {
+          Vector(prefix)
+        } else {
+          val x = digits(0) - '0'
+          map(x).flatMap(y => go(digits.tail, prefix + y))
+        }
+      }
+
+      go(digits, "").toList
+    }
+  }
+
+
+}
---
+++ src/test/scala/set000/set010/p017/SolutionSpec.scala
@@ -0,0 +1,13 @@
+package set000.set010.p017
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers {
+
+  it should "get [\"ad\", \"ae\", \"af\", \"bd\", \"be\", \"bf\", \"cd\", \"ce\", \"cf\"] when input 23" in {
+    val digits = "23"
+    val res = Solution.letterCombinations(digits)
+    val expect = List("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf").sorted
+    res.sorted shouldBe expect
+  }
+}
