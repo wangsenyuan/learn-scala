3f4b69d4c9a68c3661fac3c157a7a16d641a1c0f
WangSenyuan <brucewang@maycur.com>
2018-12-03 10:01:08 +0800

    p51

---
+++ src/test/scala/set000/set950/p951/Solution.scala
@@ -0,0 +1,23 @@
+package set000.set950.p951
+
+object Solution {
+
+  def flipEquiv(root1: TreeNode, root2: TreeNode): Boolean = {
+    if (root1 == null && root2 == null) {
+      true
+    } else if (root1 == null || root2 == null || root1.value != root2.value) {
+      false
+    } else {
+      (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) || (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left))
+    }
+  }
+}
+
+/**
+  * Definition for a binary tree node.
+  **/
+class TreeNode(var _value: Int) {
+  var value: Int = _value
+  var left: TreeNode = null
+  var right: TreeNode = null
+}
