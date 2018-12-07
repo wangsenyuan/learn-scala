8eb19609ef35dae0168a7746dc7860ac6e3ee12c
WangSenyuan <brucewang@maycur.com>
2018-12-08 00:17:33 +0800

    aaa

---
+++ src/main/leetcode/set000/set030/p037/Solution.scala
@@ -0,0 +1,77 @@
+package set000.set030.p037
+
+object Solution {
+  def solveSudoku(board: Array[Array[Char]]): Unit = {
+    val rows = Array.fill(9)(0)
+    val cols = Array.fill(9)(0)
+    val cells = Array.fill(9)(0)
+
+    def isValid(i: Int, j: Int, num: Int): Boolean = {
+      val mask = 1 << num
+
+      (rows(i) & mask) == 0 && (cols(j) & mask) == 0 && (cells(i / 3 * 3 + j / 3) & mask) == 0
+    }
+
+    def mask(i: Int, j: Int, num: Int) = {
+      val mask = 1 << num
+      rows(i) |= mask
+      cols(j) |= mask
+      cells(i / 3 * 3 + j / 3) |= mask
+    }
+
+    def unmask(i: Int, j: Int, num: Int) = {
+      val mask = 1 << num
+      rows(i) ^= mask
+      cols(j) ^= mask
+      cells(i / 3 * 3 + j / 3) ^= mask
+    }
+
+    def next(i: Int, j: Int) = {
+      if (j == 8) {
+        (i + 1, 0)
+      } else {
+        (i, j + 1)
+      }
+    }
+
+    var i = 0
+    while (i < 9) {
+      var j = 0
+      while (j < 9) {
+        if (board(i)(j) != '.') {
+          mask(i, j, board(i)(j) - '0')
+        }
+        j += 1
+      }
+
+      i += 1
+    }
+
+    def go(i: Int, j: Int): Boolean = {
+      val (u, v) = next(i, j)
+      if (i == 9) {
+        true
+      } else if (board(i)(j) != '.') {
+        go(u, v)
+      } else {
+        var num = 1
+        var ok = false
+        while (num <= 9 && !ok) {
+          if (isValid(i, j, num)) {
+            board(i)(j) = (num + '0').toChar
+            mask(i, j, num)
+            ok = go(u, v)
+            if (!ok) {
+              unmask(i, j, num)
+              board(i)(j) = '.'
+            }
+          }
+          num += 1
+        }
+        ok
+      }
+    }
+
+    go(0, 0)
+  }
+}
---
+++ src/test/scala/set000/set030/p037/SolutionSpec.scala
@@ -0,0 +1,43 @@
+package set000.set030.p037
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers {
+
+  object example1 {
+    val input =
+      """
+        |[["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+      """.stripMargin.trim
+
+    def output =
+      """
+        |[["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+      """.stripMargin.trim
+  }
+
+  def strToArray(str: String): Array[Array[Char]] = {
+    val s1 = str.substring(2, str.length - 2)
+    val ss = s1.split("\\],\\[")
+    val res = Array.fill[Array[Char]](9)(null)
+    var i = 0
+    while (i < 9) {
+      val s = ss(i)
+      val x = s.split(",")
+      res(i) = x.map(_.charAt(1))
+      i += 1
+    }
+    res
+  }
+
+  def arrayToStr(arr: Array[Array[Char]]): String = {
+    "[" + (arr.map(row => row.map(c => "\"" + c + "\"").mkString(",")).mkString("],[")) + "]"
+  }
+
+  it should "work with example1" in {
+    val arr = strToArray(example1.input)
+    Solution.solveSudoku(arr)
+    //    println(example1.output)
+    arrayToStr(arr) shouldEqual example1.output
+  }
+}
