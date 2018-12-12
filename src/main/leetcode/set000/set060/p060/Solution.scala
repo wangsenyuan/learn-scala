f788c4c6e581f8f3a81fb0b9753763660e3c9f36
WangSenyuan <brucewang@maycur.com>
2018-12-12 21:12:47 +0800

    improve performance

--- src/main/leetcode/set000/set060/p060/Solution.scala
+++ src/main/leetcode/set000/set060/p060/Solution.scala
@@ -27,26 +27,25 @@ object Solution {
       ans.mkString
     }
 
-    def firstUnUsedNum(mask: Int): Int = {
-      (0 until n).find(x => (mask & (1 << x)) == 0).get
+    def findUnUsedNums(mask: Int): Seq[Int] = {
+      (0 until n).filter(x => (mask & (1 << x)) == 0)
     }
 
     def go(m: Int, k: Int, res: Vector[Int], mask: Int): String = {
       if (k == 1) {
         getAns(res, mask)
       } else {
-        var num = firstUnUsedNum(mask)
-        var i = num
-        var cnt = 0
-        while (i < n && cnt < k) {
-          if ((mask & (1 << i)) == 0) {
-            cnt += fact(m - 1)
-            num = i
-          }
+        val nums = findUnUsedNums(mask)
+        var i = 0
+        val cnt = fact(m - 1)
+        while (i * cnt < k) {
           i += 1
         }
-        cnt -= fact(m - 1)
-        go(m - 1, k - cnt, res :+ (num + 1), mask | (1 << num))
+        // i * cnt >= k
+        i -= 1
+        val num = nums(i)
+
+        go(m - 1, k - i * cnt, res :+ (num + 1), mask | (1 << num))
       }
     }
 
