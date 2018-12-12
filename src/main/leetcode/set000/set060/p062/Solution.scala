605d66a77d89aefea1e1dab65bccedfc9a4ebaa9
WangSenyuan <brucewang@maycur.com>
2018-12-12 22:29:59 +0800

    leetcode p062

---
+++ src/main/leetcode/set000/set060/p062/Solution.scala
@@ -0,0 +1,26 @@
+package set000.set060.p062
+
+object Solution {
+
+  def uniquePaths(m: Int, n: Int): Int = {
+    val dp = Array.fill(m, n)(0)
+    dp(0)(0) = 1
+
+    var i = 0
+    while(i < m) {
+      var j = 0
+      while(j < n) {
+        if (i + 1 < m) {
+          dp(i + 1)(j) += dp(i)(j)
+        }
+        if (j + 1 < n) {
+          dp(i)(j + 1) += dp(i)(j)
+        }
+        j += 1
+      }
+      i += 1
+    }
+
+    dp(m - 1)(n - 1)
+  }
+}
