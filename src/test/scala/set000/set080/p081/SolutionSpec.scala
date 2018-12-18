42e34ed69737b7b2d855aaa04c9d23b4b39923e5
wangsenyuan <wang.senyuan@gmail.com>
2018-12-18 23:35:30 +0800

    add

---
+++ src/test/scala/set000/set080/p081/SolutionSpec.scala
@@ -0,0 +1,21 @@
+package set000.set080.p081
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers {
+
+  "2,5,6,0,0,1,2" should "find 0 success" in {
+    val res = Solution.search(Array(2, 5, 6, 0, 0, 1, 2), 0)
+    res should be(true)
+  }
+
+  it should "find 3 fail" in {
+    val res = Solution.search(Array(2, 5, 6, 0, 0, 1, 2), 3)
+    res should be(false)
+  }
+
+  "1, 1, 3, 1" should "find 3 success" in {
+    val res = Solution.search(Array(1, 1, 3, 1), 3)
+    res should be(true)
+  }
+}
