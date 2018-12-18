e59fc693ef888af91b47f3c0b5a50474c1198413
wangsenyuan <wang.senyuan@gmail.com>
2018-12-18 23:50:20 +0800

    aaa

---
+++ src/main/leetcode/set000/set080/p082/Solution.scala
@@ -0,0 +1,28 @@
+package set000.set080.p082
+
+/**
+  * Definition for singly-linked list.
+  */
+class ListNode(var _x: Int = 0) {
+  var next: ListNode = null
+  var x: Int = _x
+}
+
+object Solution {
+  def deleteDuplicates(head: ListNode): ListNode = {
+    def go(prev: ListNode, cur: ListNode): ListNode = {
+      if (cur != null) {
+        if ((prev == null || cur.x != prev.x) && (cur.next == null || cur.x != cur.next.x)) {
+          //cur will keep
+          cur.next = go(cur, cur.next)
+          cur
+        } else {
+          go(cur, cur.next)
+        }
+      } else {
+        null
+      }
+    }
+    go(null, head)
+  }
+}
