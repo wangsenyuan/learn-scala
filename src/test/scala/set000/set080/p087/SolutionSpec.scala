0efa1bc9ccb0bf006b5a1962c7302eb5f0a6fa56
WangSenyuan <brucewang@maycur.com>
2018-12-21 22:06:04 +0800

    aaa

---
+++ src/test/scala/set000/set080/p087/SolutionSpec.scala
@@ -0,0 +1,19 @@
+package set000.set080.p087
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers {
+
+  "great and rgeat" should "be scramble pairs" in {
+    Solution.isScramble("great", "rgeat") should be(true)
+  }
+
+  "abcde and caebd" should "not be scramble pairs" in {
+    Solution.isScramble("abcde", "caebd") should be(false)
+  }
+
+
+  "abc and bca" should "be scramble pairs" in {
+    Solution.isScramble("abc", "bca") should be(true)
+  }
+}
