caf3e398207aee3ca4ba7cd9f8d0a14e9f99ba28
WangSenyuan <brucewang@maycur.com>
2019-01-02 14:23:10 +0800

    leetcode p148

---
+++ src/main/leetcode/set100/set140/p147/Solution.scala
@@ -0,0 +1,37 @@
+package set100.set140.p147
+
+/**
+  * Definition for singly-linked list.
+  **/
+class ListNode(var _x: Int = 0) {
+  var next: ListNode = null
+  var x: Int = _x
+}
+
+object Solution {
+  def insertionSortList(head: ListNode): ListNode = {
+    var node = head
+    var tail: ListNode = null
+    while (node != null) {
+      val next = node.next
+
+      if (tail == null || tail.x >= node.x) {
+        node.next = tail
+        tail = node
+      } else {
+        var after: ListNode = null
+        var before = tail
+        while (before != null && before.x < node.x) {
+          after = before
+          before = before.next
+        }
+        after.next = node
+        node.next = before
+      }
+
+      node = next
+    }
+
+    tail
+  }
+}
