71a11b0e718cc9661213669acbe5a02432c6dde7
WangSenyuan <brucewang@maycur.com>
2019-02-26 22:18:51 +0800

    leetcode p394

---
+++ src/test/scala/set300/set390/p394/SolutionSpec.scala
@@ -0,0 +1,20 @@
+package set300.set390.p394
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers {
+  "3[a]2[bc]" should "be decoded to aaabcbc" in {
+    val res = Solution.decodeString("3[a]2[bc]")
+    res should equal("aaabcbc")
+  }
+
+  "3[a2[c]]" should "be decoded to accaccacc" in {
+    val res = Solution.decodeString("3[a2[c]]")
+    res should equal("accaccacc")
+  }
+
+  "2[abc]3[cd]ef" should "be decoded to abcabccdcdcdef" in {
+    val res = Solution.decodeString("2[abc]3[cd]ef")
+    res should equal("abcabccdcdcdef")
+  }
+}
