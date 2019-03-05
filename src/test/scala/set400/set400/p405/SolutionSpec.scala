5dabc973d492a6d69fc9920acb08ca6241710ce4
WangSenyuan <brucewang@maycur.com>
2019-03-05 23:13:49 +0800

    code refactor

---
+++ src/test/scala/set400/set400/p405/SolutionSpec.scala
@@ -0,0 +1,10 @@
+package set400.set400.p405
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers{
+  "-1" should "get ffffffff" in {
+    val res = Solution.toHex(-1)
+    res should be("ffffffff")
+  }
+}
