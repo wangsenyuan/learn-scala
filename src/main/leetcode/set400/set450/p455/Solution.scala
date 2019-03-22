4cf199994233577eed4ccc9152891ceb3842d5b7
WangSenyuan <brucewang@maycur.com>
2019-03-22 21:41:53 +0800

    aaa

---
+++ src/main/leetcode/set400/set450/p455/Solution.scala
@@ -0,0 +1,25 @@
+package set400.set450.p455
+
+import scala.util.Sorting
+
+object Solution {
+  def findContentChildren(g: Array[Int], s: Array[Int]): Int = {
+    Sorting.quickSort(g)
+    Sorting.quickSort(s)
+
+    var i = 0
+    var j = 0
+    var res = 0
+    while(i < g.length && j < s.length) {
+      if(s(j) >= g(i)) {
+        res += 1
+        j += 1
+        i += 1
+      } else {
+        j += 1
+      }
+    }
+
+    res
+  }
+}
---
+++ src/test/scala/set400/set450/p456/SolutionSpec.scala
@@ -0,0 +1,10 @@
+package set400.set450.p456
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers{
+  "example one" should "work" in {
+    val res = Solution.find132pattern(Array(-2, 1, 1))
+    res should be(false)
+  }
+}
