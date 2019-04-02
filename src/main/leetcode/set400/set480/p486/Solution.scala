d16b5ca23b0c8ae2815fcb4ed6979f50d198b94e
WangSenyuan <brucewang@maycur.com>
2019-04-02 09:29:09 +0800

    code refactor

--- src/main/leetcode/set400/set480/p486/Solution.scala
+++ src/main/leetcode/set400/set480/p486/Solution.scala
@@ -3,18 +3,29 @@ package set400.set480.p486
 object Solution {
   def PredictTheWinner(nums: Array[Int]): Boolean = {
     val n = nums.length
-    val sum = Array.ofDim[Int](n, n)
+    val sum = calSum(nums, n)
+
+    val dp = Array.fill(n, n)(Int.MinValue)
 
     for {
+      k <- 1 to n
       i <- 0 until n
-      j <- i until n
+      j = i + k - 1
+      if(j < n)
     } {
-      if (i == j) {
-        sum(i)(j) = nums(i)
+      if (k == 1) {
+        dp(i)(j) = sum(i)(j)
       } else {
-        sum(i)(j) = sum(i)(j - 1) + nums(j)
+        dp(i)(j) = (sum(i)(j) - dp(i + 1)(j)) max (sum(i)(j) - dp(i)(j - 1))
       }
     }
+
+    dp(0)(n - 1) * 2 >= sum(0)(n - 1)
+  }
+
+  def PredictTheWinner1(nums: Array[Int]): Boolean = {
+    val n = nums.length
+    val sum = calSum(nums, n)
 
     // dp[i][j] = the best, first player can get
     val dp = Array.fill(n, n)(-1)
@@ -37,4 +48,19 @@ object Solution {
   }
 
 
+  private def calSum(nums: Array[Int], n: Int) = {
+    val sum = Array.ofDim[Int](n, n)
+
+    for {
+      i <- 0 until n
+      j <- i until n
+    } {
+      if (i == j) {
+        sum(i)(j) = nums(i)
+      } else {
+        sum(i)(j) = sum(i)(j - 1) + nums(j)
+      }
+    }
+    sum
+  }
 }
