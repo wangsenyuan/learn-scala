eebf7f78fdd2cf7162c92cb1dd9d5f238ef2d520
WangSenyuan <brucewang@maycur.com>
2019-04-06 20:22:22 +0800

    aaa

---
+++ src/main/leetcode/set500/set500/p505/Solution.scala
@@ -0,0 +1,24 @@
+package set500.set500.p505
+
+object Solution {
+  def findRelativeRanks(nums: Array[Int]): Array[String] = {
+    val arr = nums.zipWithIndex.sortBy(_._1).reverse
+    val n = nums.length
+    val res = Array.fill(n)("")
+    for {
+      i <- arr.indices
+    } {
+      val j = arr(i)._2
+      if(i == 0) {
+        res(j) = "Gold Medal"
+      } else if(i == 1) {
+        res(j) = "Silver Medal"
+      } else if(i == 2) {
+        res(j) = "Bronze Medal"
+      } else {
+        res(j) = s"${i + 1}"
+      }
+    }
+    res
+  }
+}
