0f6da6f644abfb1bdc9b71952e7c90312f5d1d88
WangSenyuan <brucewang@maycur.com>
2019-04-07 15:47:12 +0800

    code refactor

--- src/main/leetcode/set500/set510/p514/Solution.scala
+++ src/main/leetcode/set500/set510/p514/Solution.scala
@@ -3,11 +3,56 @@ package set500.set510.p514
 import scala.collection.mutable
 
 object Solution {
+  val INF = Int.MaxValue >> 1
+
   def findRotateSteps(ring: String, key: String): Int = {
+    if (key.isEmpty) {
+      0
+    } else {
+      val m = ring.length
+      val n = key.length
+      val dp = Array.fill(m, n)(INF)
+      for {
+        i <- 0 until m
+        if (ring(i) == key(0))
+      } {
+        dp(i)(0) = (i min (m - i)) + 1
+      }
+
+      for {
+        j <- 0 until n - 1
+        i <- 0 until m
+        if dp(i)(j) < INF
+      } {
+        for {
+          k <- 0 until m
+          if ring(k) == key(j + 1)
+        } {
+          val diff1 = (k - i + m) % m
+          val diff2 = (i - k + m) % m
+          val diff = diff1 min diff2
+          dp(k)(j + 1) = dp(k)(j + 1) min (dp(i)(j) + diff + 1)
+        }
+      }
+
+      var ans = INF
+      for {
+        i <- 0 until m
+      } {
+        ans = ans min dp(i)(n - 1)
+      }
+      if (ans < INF) {
+        ans
+      } else {
+        -1
+      }
+    }
+  }
+
+  def findRotateSteps1(ring: String, key: String): Int = {
     val cache = mutable.Map.empty[Int, Int]
     val m = ring.length
     val n = key.length
-    val INF = Int.MaxValue >> 1
 
     def go(i: Int, j: Int): Int = {
       if (j == n) {
