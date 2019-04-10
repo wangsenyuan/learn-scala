fc5ddd40c97fa646f261590707a75d1c0186f35f
WangSenyuan <brucewang@maycur.com>
2019-04-10 23:12:16 +0800

    aaa

---
+++ src/main/leetcode/set500/set520/p523/Solution.scala
@@ -0,0 +1,58 @@
+package set500.set520.p523
+
+import scala.collection.mutable
+
+object Solution {
+
+  def findTwoZeros(nums: Array[Int]) = {
+    var found = false
+    var i = 1
+    while (i < nums.length && !found) {
+      found = nums(i) == 0 && nums(i - 1) == 0
+      i += 1
+    }
+    found
+  }
+
+  def checkSubarraySum(nums: Array[Int], k: Int): Boolean = {
+    val n = nums.length
+    if (n <= 1) {
+      false
+    } else {
+      val K = k.toLong.abs
+      val twoZeros = findTwoZeros(nums)
+      if (twoZeros) {
+        true
+      } else if (K == 0) {
+        false
+      } else if (K == 1) {
+        true
+      } else {
+        val pre = mutable.Map.empty[Long, Int]
+        pre(0) = -1
+        var sum = 0L
+        var found = false
+        var i = 0
+        while (i < n && !found) {
+          sum += nums(i)
+          var tmp = sum - K
+          while (tmp >= 0 && !found) {
+            if (pre.contains(tmp)) {
+              val j = pre(tmp)
+              found = i - j > 1
+            }
+            tmp -= K
+          }
+
+          if (!pre.contains(sum)) {
+            pre(sum) = i
+          }
+
+          i += 1
+        }
+
+        found
+      }
+    }
+  }
+}
---
+++ src/test/scala/set500/set520/p523/SolutionSpec.scala
@@ -0,0 +1,9 @@
+package set500.set520.p523
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers{
+  "example one" should "get true" in {
+
+  }
+}
