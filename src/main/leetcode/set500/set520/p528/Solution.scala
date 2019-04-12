6f07eb88f9cdcf1c84c8be9c5cbcbe8738660673
WangSenyuan <brucewang@maycur.com>
2019-04-12 23:40:57 +0800

    aaa

---
+++ src/main/leetcode/set500/set520/p528/Solution.scala
@@ -0,0 +1,41 @@
+package set500.set520.p528
+
+import scala.util.Random
+
+class Solution(_w: Array[Int]) {
+  val n = _w.length
+  val W = Array.ofDim[Int](n + 1)
+
+  for {
+    i <- _w.indices
+  } {
+    W(i + 1) = W(i) + _w(i)
+  }
+
+  val N = W(n)
+
+  val rand = new Random()
+
+  def pickIndex(): Int = {
+    val i = rand.nextInt(N)
+
+    val j = binarySearch(n, W(_) > i)
+    // W(j) > i
+
+    j - 1
+  }
+
+  private def binarySearch(n: Int, fn: Int => Boolean): Int = {
+    var left = 0
+    var right = n
+    while (left < right) {
+      val mid = (left + right) / 2
+      if (fn(mid)) {
+        right = mid
+      } else {
+        left = mid + 1
+      }
+    }
+    right
+  }
+}
---
+++ src/main/leetcode/set500/set520/p529/Solution.scala
@@ -0,0 +1,74 @@
+package set500.set520.p529
+
+object Solution {
+  def updateBoard(board: Array[Array[Char]], click: Array[Int]): Array[Array[Char]] = {
+    val m = board.length
+    val n = board(0).length
+
+    val res = Array.ofDim[Char](m, n)
+
+    for {
+      i <- 0 until m
+      j <- 0 until n
+    } {
+      res(i)(j) = board(i)(j)
+    }
+
+    def countMine(x: Int, y: Int) = {
+      var res = 0
+      for {
+        i <- -1 to 1
+        u = x + i
+        if (u >= 0 && u < m)
+        j <- -1 to 1
+        v = y + j
+        if (v >= 0 && v < n && (u != x || v != y))
+        if (board(u)(v) == 'M')
+      } {
+        res += 1
+      }
+
+      res
+    }
+
+    val a = click(0)
+    val b = click(1)
+    if (board(a)(b) == 'M') {
+      // boom
+      res(a)(b) = 'X'
+    } else {
+      val que = Array.ofDim[Int](m * n)
+      var front = 0
+      var end = 0
+      que(end) = a * n + b
+      end += 1
+      while (front < end) {
+        val cur = que(front)
+        front += 1
+        val x = cur / n
+        val y = cur % n
+        val cnt = countMine(x, y)
+        if (cnt == 0) {
+          res(x)(y) = 'B'
+          for {
+            i <- -1 to 1
+            u = x + i
+            if (u >= 0 && u < m)
+            j <- -1 to 1
+            v = y + j
+            if (v >= 0 && v < n && (u != x || v != y))
+            if (res(u)(v) == 'E')
+          } {
+            res(u)(v) = 'F'
+            que(end) = u * n + v
+            end += 1
+          }
+        } else {
+          res(x)(y) = ('0' + cnt).toChar
+        }
+      }
+    }
+
+    res
+  }
+}
