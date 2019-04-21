958abfd1e8e9e5b6e85d863ba53ff1d3f0d0e222
WangSenyuan <brucewang@maycur.com>
2019-04-21 22:02:11 +0800

    aaa

---
+++ src/main/leetcode/set1000/set1030/p1032/StreamChecker.scala
@@ -0,0 +1,39 @@
+package set1000.set1030.p1032
+
+class StreamChecker(_words: Array[String]) {
+  val root = new Trie
+
+  _words.foreach(word => root.add(word))
+
+  var cur = Array.empty[Trie]
+
+  def query(letter: Char): Boolean = {
+    cur = cur.map(_.query(letter)).filter(_ != null)
+    val next = root.query(letter)
+    if(next != null) {
+      cur = cur :+ next
+    }
+    cur.exists(_.leaf)
+  }
+
+  class Trie {
+    var leaf = false
+    val children = Array.ofDim[Trie](26)
+
+    def add(word: String): Unit = {
+      if(word.isEmpty) {
+        this.leaf = true
+      } else {
+        val i = word.head - 'a'
+        if(children(i) == null) {
+          children(i) = new Trie
+        }
+        children(i).add(word.tail)
+      }
+    }
+
+    def query(letter: Char): Trie = {
+      children(letter - 'a')
+    }
+  }
+}
