7eef26fdc40983c343b57c55f29e8d4e57e1dc28
WangSenyuan <brucewang@maycur.com>
2019-04-27 14:53:42 +0800

    aa

--- src/main/leetcode/set500/set590/p594/Solution.scala
+++ src/main/leetcode/set500/set590/p594/Solution.scala
@@ -1,9 +1,31 @@
 package set500.set590.p594
 
+import scala.collection.mutable
 import scala.util.Sorting
 
 object Solution {
   def findLHS(nums: Array[Int]): Int = {
+    val count = mutable.Map.empty[Int, Int].withDefaultValue(0)
+
+    for {
+      num <- nums
+    } {
+      count(num) += 1
+    }
+
+    var best = 0
+
+    for {
+      (k, v) <- count
+      if(count.contains(k - 1))
+    } {
+      best = best max (v + count(k - 1))
+    }
+
+    best
+  }
+
+  def findLHS1(nums: Array[Int]): Int = {
     Sorting.quickSort(nums)
     val n = nums.length
     var best = 0
