31c1449a29028d5d82941846cb38dab868d78e53
WangSenyuan <brucewang@maycur.com>
2019-04-28 09:02:35 +0800

    aaa

---
+++ src/main/leetcode/set500/set590/p598/Solution.scala
@@ -0,0 +1,16 @@
+package set500.set590.p598
+
+object Solution {
+  def maxCount(m: Int, n: Int, ops: Array[Array[Int]]): Int = {
+    var row = m
+    var col = n
+    for {
+      op <- ops
+    } {
+      row = row min op(0)
+      col = col min op(1)
+    }
+
+    row * col
+  }
+}
