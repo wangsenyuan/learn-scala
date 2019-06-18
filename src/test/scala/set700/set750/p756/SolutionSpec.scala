866bec63ab201d05cc4f39eb08032beaca28a9a6
WangSenyuan <brucewang@maycur.com>
2019-06-18 09:56:51 +0800

    aaa

---
+++ src/test/scala/set700/set750/p756/SolutionSpec.scala
@@ -0,0 +1,10 @@
+package set700.set750.p756
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers {
+  "example one" should "work" in {
+    val res = Solution.pyramidTransition("AABA", List("AAA", "AAB", "ABA", "ABB", "BAC"))
+    res should be(false)
+  }
+}
