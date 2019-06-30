dbc53c40c3a11eea744141f9fc130b4606d19a0b
WangSenyuan <brucewang@maycur.com>
2019-06-30 22:38:25 +0800

    aaa

---
+++ src/test/scala/set700/set770/p777/SolutionSpec.scala
@@ -0,0 +1,19 @@
+package set700.set770.p777
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers {
+  "example one" should "work" in {
+    val start = "RXXLRXRXL"
+    val end = "XRLXXRRLX"
+    val res = Solution.canTransform(start, end)
+    res should be(true)
+  }
+
+  "example two" should "work" in {
+    val start = "RL"
+    val end = "LR"
+    val res = Solution.canTransform(start, end)
+    res should be(false)
+  }
+}
