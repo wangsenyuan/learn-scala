679ff8e19ef40dc22900894e2667ff457a09793f
WangSenyuan <brucewang@maycur.com>
2019-07-05 23:24:50 +0800

    aaaa

---
+++ src/test/scala/set700/set780/p781/SolutionSpec.scala
@@ -0,0 +1,29 @@
+package set700.set780.p781
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers {
+  "example one" should "work" in {
+    val ans = Array(1, 1, 0, 0, 0)
+    val res = Solution.numRabbits(ans)
+    res should be(5)
+  }
+
+  "example two" should "work" in {
+    val ans = Array(10, 10, 10)
+    val res = Solution.numRabbits(ans)
+    res should be(11)
+  }
+
+  "example three" should "work" in {
+    val ans = Array(1, 1, 2)
+    val res = Solution.numRabbits(ans)
+    res should be(5)
+  }
+
+  "example four" should "work" in {
+    val ans = Array(1, 1, 0, 0, 1)
+    val res = Solution.numRabbits(ans)
+    res should be(6)
+  }
+}
