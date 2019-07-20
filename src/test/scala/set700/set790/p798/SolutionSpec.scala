f656f7b43263efec3053dc7c0d060ecce3dcc184
WangSenyuan <brucewang@maycur.com>
2019-07-20 21:18:23 +0800

    aaa

---
+++ src/test/scala/set700/set790/p798/SolutionSpec.scala
@@ -0,0 +1,11 @@
+package set700.set790.p798
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers {
+  "example one" should "work" in {
+    val arr = Array(2, 3, 1, 4, 0)
+    val res = Solution.bestRotation(arr)
+    res should be(3)
+  }
+}
