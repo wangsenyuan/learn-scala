6b4e3a4dd94a6998d596f8a49ccbbc72b2cb4667
WangSenyuan <brucewang@maycur.com>
2019-07-24 20:02:24 +0800

    aaa

---
+++ src/main/leetcode/set800/set800/p804/Solution.scala
@@ -0,0 +1,19 @@
+package set800.set800.p804
+
+import scala.collection.mutable
+
+object Solution {
+  val codes = Array(".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..")
+
+  def uniqueMorseRepresentations(words: Array[String]): Int = {
+    val res = mutable.Set.empty[String]
+
+    words.foreach(word => res += transform(word))
+
+    res.size
+  }
+
+  private def transform(word: String): String = {
+    word.map(c => codes(c - 'a')).mkString("")
+  }
+}
