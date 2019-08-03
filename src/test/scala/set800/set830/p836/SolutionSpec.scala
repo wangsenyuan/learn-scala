d8ade4be084779a27c12f3985dba0fd23fabbc49
WangSenyuan <brucewang@maycur.com>
2019-08-03 21:51:28 +0800

    aaa

---
+++ src/test/scala/set800/set830/p836/SolutionSpec.scala
@@ -0,0 +1,10 @@
+package set800.set830.p836
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers{
+  "example one" should "work" in {
+    val res = Solution.isRectangleOverlap(Array(-6,-10,9,2), Array(0,5,4,8))
+    res should be(false)
+  }
+}
