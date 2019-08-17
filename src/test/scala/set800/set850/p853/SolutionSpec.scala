5bfa072d54621e5672a3edb2effd79b67c9ddf0b
WangSenyuan <brucewang@maycur.com>
2019-08-17 19:50:48 +0800

    aaa

---
+++ src/test/scala/set800/set850/p853/SolutionSpec.scala
@@ -0,0 +1,21 @@
+package set800.set850.p853
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers {
+  "example one" should "work" in {
+    val target = 12
+    val position = Array(10, 8, 0, 5, 3)
+    val speed = Array(2, 4, 1, 1, 3)
+    val res = Solution.carFleet(target, position, speed)
+    res should be(3)
+  }
+
+  "example two" should "work" in {
+    val target = 10
+    val position = Array(0, 4, 2)
+    val speed = Array(2, 1, 3)
+    val res = Solution.carFleet(target, position, speed)
+    res should be(1)
+  }
+}
