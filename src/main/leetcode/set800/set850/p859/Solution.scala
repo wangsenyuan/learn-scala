3ab2788c925492ae68bd5e883b61f391427c3c8f
WangSenyuan <brucewang@maycur.com>
2019-08-21 21:20:04 +0800

    p860

---
+++ src/main/leetcode/set800/set850/p859/Solution.scala
@@ -0,0 +1,37 @@
+package set800.set850.p859
+
+object Solution {
+  def buddyStrings(A: String, B: String): Boolean = {
+    if (A.length != B.length) {
+      false
+    } else {
+      val n = A.length
+      var i = 0
+      while (i < n && A(i) == B(i)) {
+        i += 1
+      }
+
+      if (i == n) {
+        A.groupBy(identity).exists(_._2.size > 1)
+      } else {
+        var j = i + 1
+        while (j < n && A(j) == B(j)) {
+          j += 1
+        }
+        if (j == n) {
+          false
+        } else {
+          if (A(i) != B(j) || A(j) != B(i)) {
+            false
+          } else {
+            j += 1
+            while (j < n && A(j) == B(j)) {
+              j += 1
+            }
+            j == n
+          }
+        }
+      }
+    }
+  }
+}
---
+++ src/main/leetcode/set800/set860/p860/Solution.scala
@@ -0,0 +1,34 @@
+package set800.set860.p860
+
+object Solution {
+  def lemonadeChange(bills: Array[Int]): Boolean = {
+    val hand = Array(0, 0)
+
+    var i = 0
+    while (i < bills.length) {
+      val x = bills(i) / 5
+      if (x == 1) {
+        hand(0) += 1
+      } else if (x == 2) {
+        if (hand(0) == 0) {
+          return false
+        }
+        hand(0) -= 1
+        hand(1) += 1
+      } else {
+        // 20
+        if (hand(1) > 0 && hand(0) > 0) {
+          hand(1) -= 1
+          hand(0) -= 1
+        } else if (hand(0) >= 3) {
+          hand(0) -= 3
+        } else {
+          return false
+        }
+      }
+      i += 1
+    }
+
+    true
+  }
+}
