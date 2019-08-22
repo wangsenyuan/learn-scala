4159e103c155379873f38f50de4b19e6d83c1360
WangSenyuan <brucewang@maycur.com>
2019-08-22 13:41:30 +0800

    code refactor

---
+++ src/test/scala/set800/set860/p862/SolutionSpec.scala
@@ -0,0 +1,33 @@
+package set800.set860.p862
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers {
+  "example one" should "work" in {
+    val A = Array(1)
+    val K = 1
+    val res = Solution.shortestSubarray(A, K)
+    res should be(1)
+  }
+
+  "example two" should "work" in {
+    val A = Array(1, 2)
+    val K = 4
+    val res = Solution.shortestSubarray(A, K)
+    res should be(-1)
+  }
+
+  "example three" should "work" in {
+    val A = Array(2, -1, 2)
+    val K = 3
+    val res = Solution.shortestSubarray(A, K)
+    res should be(3)
+  }
+
+  "example four" should "work" in {
+    val A = Array(17, 85, 93, -45, -21)
+    val K = 150
+    val res = Solution.shortestSubarray(A, K)
+    res should be(2)
+  }
+}
