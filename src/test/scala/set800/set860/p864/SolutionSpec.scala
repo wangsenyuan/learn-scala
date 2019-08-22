d069011643ec5b97f3b19b0b2cb0f79c9dc32256
WangSenyuan <brucewang@maycur.com>
2019-08-22 22:14:40 +0800

    aaa

---
+++ src/test/scala/set800/set860/p864/SolutionSpec.scala
@@ -0,0 +1,23 @@
+package set800.set860.p864
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SolutionSpec extends FlatSpec with Matchers {
+  "example one" should "work" in {
+    val grid = Array("@.a.#", "###.#", "b.A.B")
+    val res = Solution.shortestPathAllKeys(grid)
+    res should be(8)
+  }
+
+  "example two" should "work" in {
+    val grid = Array("@..aA", "..B#.", "....b")
+    val res = Solution.shortestPathAllKeys(grid)
+    res should be(6)
+  }
+
+  "example three" should "work" in {
+    val grid = Array("@...a", ".###A", "b.BCc")
+    val res = Solution.shortestPathAllKeys(grid)
+    res should be(10)
+  }
+}
